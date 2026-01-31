128e0085fafbb4b3a9cb11bb887ddbf0d9984ae5
themirrazz <89490659+themirrazz@users.noreply.github.com>
2026-01-31 11:53:56 -0800

    sometimes pkgbuild is stupid

---
+++ aur/libdir-patch.js
@@ -0,0 +1,13 @@
+const path = require('path');
+module.exports = {
+    // Files
+    gameAsar: path.join(__dirname, 'game.asar'),
+    gameLock: path.join(__dirname, 'game-version.lock'),
+    pepperFlash: '/lib/PepperFlash/libpepflashplayer.so',
+    // Optional modules
+    libinstall: './libinstall.js',
+    everyUpdater: './everyUpdater.js',
+    electronUpdater: './game.asar/node_modules/electron-updater',
+    gameConfig: './game.asar/config.js',
+    gameEntry: './game.asar/index.js',
+};
