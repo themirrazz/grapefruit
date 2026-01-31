e0379c6d1e7d63f3d27f2110c6b6c1175ca0f7cd
themirrazz <89490659+themirrazz@users.noreply.github.com>
2026-01-31 12:04:03 -0800

    i cant spell

---
+++ aur/libdir-patch-icantspell.js
@@ -0,0 +1,13 @@
+const path = require('path');
+module.exports = {
+    // Files
+    gameAsar: '/usr/lib/ajclassic-grapefruit/game.asar',
+    gameLock: '/usr/lib/ajclassic-grapefruit/game-version.lock',
+    pepperFlash: '/lib/PepperFlash/libpepflashplayer.so',
+    // Optional modules
+    libinstall: './libinstall.js',
+    everyUpdater: './everyUpdater.js',
+    electronUpdater: './game.asar/node_modules/electron-updater',
+    gameConfig: './game.asar/config.js',
+    gameEntry: './game.asar/index.js',
+};
