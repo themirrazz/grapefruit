c7ecba9f2482916ac6e3eb83631f0cd10a1d158b
themirrazz <89490659+themirrazz@users.noreply.github.com>
2026-01-31 12:09:47 -0800

    wtf is wrong with nodejs

---
+++ aur/libdir-final-fix.js
@@ -0,0 +1,13 @@
+const path = require('path');
+module.exports = {
+    // Files
+    gameAsar: '/usr/lib/ajclassic-grapefruit/game.asar',
+    gameLock: '/usr/lib/ajclassic-grapefruit/game-version.lock',
+    pepperFlash: '/lib/PepperFlash/libpepflashplayer.so',
+    // Optional modules
+    libinstall: '/usr/lib/ajclassic-grapefruit/libinstall.js',
+    everyUpdater: '/usr/lib/ajclassic-grapefruit/everyUpdater.js',
+    electronUpdater: '/usr/lib/ajclassic-grapefruit/game.asar/node_modules/electron-updater',
+    gameConfig: '/usr/lib/ajclassic-grapefruit/game.asar/config.js',
+    gameEntry: '/usr/lib/ajclassic-grapefruit/game.asar/index.js',
+};
